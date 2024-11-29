e671b03c628f772b4ebd25947e0be32084680a5c
eberhaan <antonia.eberharter@umit-tirol.at>
2024-11-29 13:26:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -335,28 +335,6 @@ $(function() {
   }
   
 
-  // The variable QueryString contains the url parameters, i.e. condition no. and participant no.
-  // via http://stackoverflow.com/a/979995
-  window.QueryString = function () {
-    var query_string = {};
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i=0;i<vars.length;i++) {
-      var pair = vars[i].split("=");
-        // If first entry with this name
-      if (typeof query_string[pair[0]] === "undefined") {
-        query_string[pair[0]] = pair[1];
-        // If second entry with this name
-      } else if (typeof query_string[pair[0]] === "string") {
-        var arr = [ query_string[pair[0]], pair[1] ];
-        query_string[pair[0]] = arr;
-        // If third or later entry with this name
-      } else {
-        query_string[pair[0]].push(pair[1]);
-      }
-    } 
-      return query_string;
-  } ();
 
 
   // Function to check letters and numbers
